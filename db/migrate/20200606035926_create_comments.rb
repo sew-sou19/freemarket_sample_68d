f585777929c9bcc7eefa34f282cb52baba579ec7
touikoba <toui.kba@gmail.com>
2020-06-25 20:35:41 +0900

    変更しました

--- db/migrate/20200606035926_create_comments.rb
+++ db/migrate/20200606035926_create_comments.rb
@@ -1,10 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.string :content
+      t.references :content
       t.references :item, foreign_key: true
       t.references :user_id, foreign_key: true
-      t.text :text
       t.timestamps
       
     end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :comment do
     content { "MyString" }
-    user { nil }
+
   end
 end
