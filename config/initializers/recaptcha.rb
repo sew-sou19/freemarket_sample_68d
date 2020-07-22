f7651d7bfee0c7a10b4d392dfa1085f69191737e
MugiOka <okamugi0722@gmail.com>
2020-07-23 00:19:27 +0900

    complete

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,4 @@
-# Recaptcha.configure do |config|
-#   ENV['RECAPTCHA_SITE_KEY']   = Rails.application.credentials.recaptcha[:public_key]
-#   ENV['RECAPTCHA_SECRET_KEY'] = Rails.application.credentials.recaptcha[:private_key]
-#   config.site_key  = Rails.application.credentials.recaptcha[:public_key]
-#   config.secret_key = Rails.application.credentials.recaptcha[:private_key]
-# end
+Recaptcha.configure do |config|
+  config.site_key  = Rails.application.credentials.recaptcha[:public_key]
+  config.secret_key = Rails.application.credentials.recaptcha[:private_key]
+end
